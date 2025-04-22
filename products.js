0a488e72ddf02ae68e123cc4d65399f7bed6ac81
kian0401 <kianandrew4@gmail.com>
2025-04-22 23:15:05 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,87 @@
+const products = [
+    {
+        id: 1,
+        title: "Unisex Kids Tees",
+        price: "R150",
+        description: "Plain Tees never go out of style plus they are made from one of the best farbic out there.",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images1/product101.jpg",
+                thumbnails: [
+                    "images1/product101.jpg",
+                    "images1/product101.jpg",
+                    "images1/product101.jpg"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+            {
+                name: "Blue",
+                mainImage: "images1/Product11 blue.jpg",
+                thumbnails: [
+                    "images1/Product11 blue.jpg",
+                    "images1/Product11 blue.jpg",
+                    "images1/Product11 blue.jpgf"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+            {
+                name: "White",
+                mainImage: "images1/Product 1 white.jpg",
+                thumbnails: [
+                    "images1/Product 1 white.jpg",
+                    "images1/Product 1 white.jpg",
+                    "images1/Product 1 white.jpg"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+           
+            
+        ]
+    },
+    {
+        id: 2,
+        title: "Long sleeve",
+        price: "R200",
+        description: "Winter is approaching keep your kids warm and comfy with our latest long sleeve shirts made from the highest frabric quality.",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images1/product 2 black.jpg",
+                thumbnails: [
+                    "images1/product 2 black.jpg",
+                    "images1/product 2 black.jpg",
+                    "images1/product 2 black.jpg"
+                ],
+                sizes: ["M", "L", "XL"]
+            },
+
+            {
+                name: "Blue",
+                mainImage: "images1/Product 2 blue.jpg",
+                thumbnails: [
+                    "images1/Product 2 blue.jpg",
+                    "images1/Product 2 blue.jpg",
+                    "images1/Product 2 blue.jpg"
+                ],
+                sizes: ["M", "L", "XL"]
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Unisex graphic tee",
+        price: "R100",
+        description: "Style your kids with this epic graphic tee design.",
+        colors: [
+            {
+                name: "Yellow",
+                mainImage: "images1/product 3.jpg",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+    },
+    
+   
+];
